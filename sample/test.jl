4101a8c12d501144c3048103e068d55d7ecc1540
Vincent Le Chenadec <vincentlechenadec@gmail.com>
2021-04-18 21:05:25 +0200

    added a sample notebook

---
+++ sample/test.jl
@@ -0,0 +1,27 @@
+### A Pluto.jl notebook ###
+# v0.12.20
+
+using Markdown
+using InteractiveUtils
+
+# ╔═╡ ab5846b4-a078-11eb-3b05-3322379dd93e
+md"""
+This is a sample notebook using ``\LaTeX`` math.
+```math
+\exp \colon x \mapsto \sum_{n = 0} ^ \infty \frac{x ^ n}{n !}.
+```
+
+And a "Hello, World!" example.
+
+"""
+
+# ╔═╡ d5096696-a078-11eb-0219-a93d9a7ed0b6
+greet(name = "World") = "Hello, $(name)!"
+
+# ╔═╡ dfba57bc-a078-11eb-05ba-bfa2dafa85ea
+greet()
+
+# ╔═╡ Cell order:
+# ╟─ab5846b4-a078-11eb-3b05-3322379dd93e
+# ╠═d5096696-a078-11eb-0219-a93d9a7ed0b6
+# ╠═dfba57bc-a078-11eb-05ba-bfa2dafa85ea
